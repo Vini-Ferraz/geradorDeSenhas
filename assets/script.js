2d42c3679ddb5178557c73f2feadf8859ed5ffef
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2023-11-25 17:42:08 -0300

    Update Js

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,19 @@
+// Função para gerar senha
 function gerarSenha() {
+    // Arrays com os caracteres possíveis
     let senha = [];
     const alfabetoMaiusculo = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     const alfabetoMinusculo = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     const simbolosEspeciais = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ';', ':', '<', '>', ',', '.', '?', '/'];
-
+    // Obter as opções selecionadas pelo usuário
     let maisculaCheckBox = document.getElementById('letrasMaiusculas');
     let minusculaCheckBox = document.getElementById('letrasMinusculas');
     let numerosCheckBox = document.getElementById('numeros');
     let simbolosEspeciaisCheckBox = document.getElementById('simbolosEspeciais');
     let tamanhoDaSenha = document.getElementById('tamanhoSenha').value;
     let campoSenha = document.getElementById('senhaInput');
-
+    // Loop para construir a senha
     for (let i = 0; i <= tamanhoDaSenha; i++){
         if (maisculaCheckBox.checked) {
             let num = Math.floor(Math.random() * (alfabetoMaiusculo.length - 0)) + 0;
@@ -30,6 +32,7 @@ function gerarSenha() {
             senha.push(simbolosEspeciais[num]);
         }
     }
+    // Função para embaralhar a senha
     function embaralharSenha(arr){
         let senhaEmbaralhada = arr.slice();
         for (let i  = 0; i < senhaEmbaralhada.length; i++){
@@ -38,11 +41,12 @@ function gerarSenha() {
             return senhaEmbaralhada;
         }
     }
+    // Embaralhar a senha e obter a versão final
     let senhaMisturada = embaralharSenha(senha);
     let senhaFinal = senhaMisturada.slice(0, tamanhoDaSenha).join("");
     campoSenha.value = senhaFinal;
 }
-
+// Obter o botão e adicionar um ouvinte de eventos para gerar senha ao clicar
 let senhaBtn = document.getElementById('btnGerarSenha');
 senhaBtn.addEventListener('click', function() {
     gerarSenha();
