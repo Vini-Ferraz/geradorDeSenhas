13369108fe3297c732050723f960df9e053331fd
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2024-01-08 11:25:09 -0300

    CRIANDO FUNCIONALIDADE DO POP UP
    CRIEI A FUNCIONALIDADE DO JS PARA O POP UP.
    
    Caso a API do ClipBoard retorne resultado positivo, o pop ira aparecer.

--- assets/scriptCopiarSenha.js
+++ assets/scriptCopiarSenha.js
@@ -1,9 +1,17 @@
+/*Pop Up*/
+let popUp = document.getElementById("popUpPositivo");
+function removerPopUp() {
+  popUp.classList.remove("popUpPositivoRevelar");
+}
+
 function copiarSenha() {
   let text = document.getElementById("senhaInput").textContent;
 
   const copyContent = async () => {
     try {
       await navigator.clipboard.writeText(text);
+      popUp.classList.add("popUpPositivoRevelar");
+      setTimeout(removerPopUp, 2500);
     } catch (err) {
       console.error("Failed to copy: ", err);
     }
@@ -14,7 +22,3 @@ let btnCopiar = document.getElementById("btnCopia");
 btnCopiar.addEventListener("click", function () {
   copiarSenha();
 });
-
-/*Pop Up*/
-
-let popUpPositivo = document.getElementById("popUpPositivo");
