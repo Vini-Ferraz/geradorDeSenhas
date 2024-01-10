f4359ce34dfa57c6c84402b04c38f51a58119032
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2024-01-10 11:06:25 -0300

    COMENTANTO SCRIPT DE COPIAR
    COMENTEI COMO O SCRIPT DE COPIAR A SENHA FUNCIONA.

--- assets/scriptCopiarSenha.js
+++ assets/scriptCopiarSenha.js
@@ -1,18 +1,22 @@
-/*Pop Up*/
+// Obtém a referência do elemento HTML com o ID "popUpPositivo"
 let popUp = document.getElementById("popUpPositivo");
+// Função para remover a classe que revela o pop-up
 function removerPopUp() {
   popUp.classList.remove("popUpPositivoRevelar");
 }
-
+// Função para copiar a senha
 function copiarSenha() {
   let text = document.getElementById("senhaInput").textContent;
-
+  // Função assíncrona para copiar o conteúdo para a área de transferência
   const copyContent = async () => {
     try {
+      // Utiliza a API do Clipboard para escrever o texto na área de transferência
       await navigator.clipboard.writeText(text);
+      // Adiciona a classe que revela o pop-up positivo
       popUp.classList.add("popUpPositivoRevelar");
       setTimeout(removerPopUp, 2500);
     } catch (err) {
+      // Exibe um alerta em caso de falha na cópia
       alert("Failed to copy: ", err);
     }
   };
